af06cd204daab2929152be39952044d875d96cdb
Je Sen <jesen_teh@usm.my>
2021-11-15 20:37:32 +0800

    Add files via upload

--- cryptanalysis/boomerang-notimestamp.py
+++ cryptanalysis/boomerang-notimestamp.py
@@ -41,10 +41,10 @@ def computeFeistelBoomerangDifferential(cipher, parameters):
         print("Running initial boomerang search")
         print("----")
         #Finds the input and output differences of the entire boomerang then starts enumerating
-        boomerangProb = feistelBoomerangTrailSearch(cipher, parameters, start_time)
+        boomerangProb = feistelBoomerangTrailSearch(cipher, parameters)
         #Compute other boomerang trails for the given input and output differences
         while not search.reachedTimelimit(start_time, parameters["timelimit"]):
-            prob = feistelBoomerangTrailSearch(cipher, parameters, start_time, boomerangProb)
+            prob = feistelBoomerangTrailSearch(cipher, parameters, boomerangProb)
             if prob == 99: #No more upper trails for the given input
                 break
             elif prob == 0: #No lower trail found for the given limits
@@ -66,106 +66,7 @@ def computeFeistelBoomerangDifferential(cipher, parameters):
     return 0
 
 
-def feistelBoomerangTrailSearch(cipher, parameters, timestamp, boomerangProb = 0):
-    """
-    Automatically enumerate boomerang differentials starting from a fixed upper trail
-    """
-    switchProb = 0
-    alpha = ""
-    beta = ""
-    delta = ""
-    gamma = ""
-    upperCharacteristic = ""
-    lowerCharacteristic = "" 
-    diff_upper = 0
-    diff_lower = 0
-    start_time = timestamp
-        
-    #Search Upper Trail
-    upperCharacteristic = boomerangTrail(cipher, parameters, timestamp, "upper")
-
-    #Store output difference
-    try:
-        alpha = upperCharacteristic.getInputDiff()
-        beta = upperCharacteristic.getOutputDiff()
-    except:
-        print("No characteristic found for the given limits")
-        #If no more upper characteristics can be found, best boomerang differential for the given input has been found
-        parameters["uweight"] = parameters["sweight"]
-        parameters["blockedUpperCharacteristics"].append(upperCharacteristic)
-        parameters["blockedLowerCharacteristics"].clear()
-        return 99
-    upperWeight = parameters["sweight"] #Store optimal weight found for upper trail
-
-    #Keep searching for another optimal lower characteristic, otherwise move on to different upper
-    lowerWeight = parameters["lweight"]
-
-    #Calculate weight limit of lower trails
-    if parameters["lweight"] < parameters["wordsize"]/parameters["sboxSize"]:
-        searchLimit = parameters["wordsize"]/parameters["sboxSize"] - parameters["lweight"]
-    else:
-        searchLimit = 1 #+1 means only find optimal lower trails 
-
-    while not search.reachedTimelimit(start_time, parameters["timelimit"]) and \
-        lowerWeight < parameters["lweight"]+searchLimit: 
-
-        #Search Lower Trail
-        lowerCharacteristic = boomerangTrail(cipher, parameters, timestamp, "lower", beta)
-
-        #Store output difference
-        try:
-            gamma = lowerCharacteristic.getInputDiff()
-            delta = lowerCharacteristic.getOutputDiff()
-        except:
-            print("No characteristic found for the given limits")
-            parameters["blockedUpperCharacteristics"].append(upperCharacteristic)   
-            parameters["blockedLowerCharacteristics"].clear()
-            return 0
-        lowerWeight = parameters["sweight"] #Store optimal weight found for lower trail
-
-        #Block characteristics
-        parameters["blockedLowerCharacteristics"].append(lowerCharacteristic)
-
-        #Check for match
-        switchProb = checkBCT(beta, gamma, parameters, cipher)
-
-        #Successful switch has been found
-        if switchProb != 0: 
-            #Fix starting point if it has not been set in boomerang Variables
-            if "X0" not in parameters["boomerangVariables"]:
-                parameters["boomerangVariables"]["X0"] = alpha
-                print("Fixed X0 in boomerang to {}".format(parameters["boomerangVariables"]["X0"]))
-                print("----")
-            #Fix end point if it has not been set in boomerang Variables
-            if "X{}".format(parameters["lowertrail"]) not in parameters["boomerangVariables"]:
-                parameters["boomerangVariables"]["X{}".format(parameters["lowertrail"])] = delta
-                print("Fixed X{} in boomerang to {}".format(parameters["lowertrail"], parameters["boomerangVariables"]["X{}".format(parameters["lowertrail"])]))
-                print("----")
-            #Perform clustering for upper if not done, then cluster lower
-            while diff_upper == 0:
-                diff_upper = boomerangDifferential(cipher, parameters, alpha, beta, upperWeight, timestamp, "upper")
-            diff_lower = 0
-            while diff_lower == 0:
-                diff_lower = boomerangDifferential(cipher, parameters, gamma, delta, lowerWeight, timestamp, "lower")
-            boomerangProb += diff_upper*diff_upper*diff_lower*diff_lower*switchProb
-            print("Found boomerang trail: {}, {}, {}".format(math.log(diff_upper, 2), math.log(diff_lower, 2),math.log(switchProb, 2)))
-            print("Boomerang probability: {}".format(math.log(boomerangProb, 2)))
-            print("----")
-        else:
-            print("Invalid switch, search for new boomerang differential")
-            print("----")
-   
-    #After searching for all possible optimal lower trails for the given upper trail, block upper trail
-    print("Completed trail search with boomerang probability of {}".format(math.log(boomerangProb, 2)))
-    #Block upper trail to find another upper trail
-    parameters["blockedUpperCharacteristics"].append(upperCharacteristic)
-    #Clear lower trails because the same lower trails can be matched to a different upper trail
-    parameters["blockedLowerCharacteristics"].clear()
-    parameters["uweight"] = upperWeight
-    return boomerangProb
-
-
-def boomerangTrail(cipher, parameters, timestamp, boomerangFace="upper", switchInput=""):
+def boomerangTrail(cipher, parameters, boomerangFace="upper", switchInput=""):
     """
     Search top or bottom trail (characteristic) of a boomerang
     """
@@ -189,12 +90,10 @@ def boomerangTrail(cipher, parameters, timestamp, boomerangFace="upper", switchI
                                                  parameters[trail],
                                                  parameters["wordsize"])))
     print("---")
-    start_time = timestamp
+    start_time = time.time()
 
     #Set target weight for trail
     parameters["sweight"] = parameters[weight]
-    characteristic = ""
-    
     while not search.reachedTimelimit(start_time, parameters["timelimit"]) and \
         parameters["sweight"] < parameters["endweight"]:
 
@@ -202,8 +101,8 @@ def boomerangTrail(cipher, parameters, timestamp, boomerangFace="upper", switchI
                                             round(time.time() - start_time, 2)))
 
         # Construct problem instance for given parameters
-        stp_file = "tmp/{}-{}{}-{}-{}.stp".format(boomerangFace, cipher.name,
-                                         parameters["wordsize"], parameters[trail], timestamp)
+        stp_file = "tmp/{}-{}{}-{}.stp".format(boomerangFace, cipher.name,
+                                         parameters["wordsize"], parameters[trail])
         
         #Fix number of rounds
         parameters["rounds"] = parameters[trail]
@@ -252,15 +151,10 @@ def boomerangTrail(cipher, parameters, timestamp, boomerangFace="upper", switchI
             break
         parameters["sweight"] += 1
         print("----")
-
-    if parameters["sweight"] >= parameters["endweight"] and boomerangFace == "upper":
-        print("Weight limit has been reached. Ending search.")
-        quit()
-
     return characteristic
 
 
-def boomerangDifferential(cipher, parameters, input, output, weight, timestamp, boomerangFace="upper"):
+def boomerangDifferential(cipher, parameters, input, output, weight, boomerangFace="upper"):
     """
     Perform clustering for one face of a boomerang differential
     """
@@ -272,19 +166,17 @@ def boomerangDifferential(cipher, parameters, input, output, weight, timestamp, 
         trail = "lowertrail"
         limit = "lowerlimit"
 
-    start_time = timestamp
-    
     print("Cluster {} differential".format(boomerangFace))
     
     #Clear blocked characteristics
     parameters["blockedCharacteristics"].clear()
 
     #Setup search
-    #rnd_string_tmp = '%030x' % random.randrange(16**30)
+    rnd_string_tmp = '%030x' % random.randrange(16**30)
     diff_prob = 0
     boomerangProb = 1
     characteristics_found = 0
-    sat_logfile = "tmp/satlog{}.tmp".format(timestamp)
+    sat_logfile = "tmp/satlog{}.tmp".format(rnd_string_tmp)
 
     parameters["fixedVariables"].clear()
     parameters["fixedVariables"]["X0"] = input
@@ -298,6 +190,8 @@ def boomerangDifferential(cipher, parameters, input, output, weight, timestamp, 
     #Fix number of rounds
     parameters["rounds"] = parameters[trail]
 
+    start_time = time.time()
+
     #Search until optimal weight + wordsize/8
     while not search.reachedTimelimit(start_time, parameters["timelimit"]) and \
         parameters["sweight"] < weight+parameters["wordsize"]/parameters[limit]:
@@ -305,7 +199,7 @@ def boomerangDifferential(cipher, parameters, input, output, weight, timestamp, 
         if os.path.isfile(sat_logfile):
             os.remove(sat_logfile)
 
-        stp_file = "tmp/{}{}-{}.stp".format(cipher.name, trail,timestamp)
+        stp_file = "tmp/{}{}.stp".format(cipher.name, trail)
         cipher.createSTP(stp_file, parameters)
 
         # Start solver
@@ -347,13 +241,119 @@ def boomerangDifferential(cipher, parameters, input, output, weight, timestamp, 
     return diff_prob
 
 
+def feistelBoomerangTrailSearch(cipher, parameters, boomerangProb = 0):
+    """
+    Automatically enumerate boomerang differentials starting from a fixed upper trail
+    """
+    switchProb = 0
+    alpha = ""
+    beta = ""
+    delta = ""
+    gamma = ""
+    upperCharacteristic = ""
+    lowerCharacteristic = "" 
+    diff_upper = 0
+    diff_lower = 0
+    start_time = time.time()
+        
+    #Search Upper Trail
+    upperCharacteristic = boomerangTrail(cipher, parameters, "upper")
+
+    #Store output difference
+    try:
+        alpha = upperCharacteristic.getInputDiff()
+        beta = upperCharacteristic.getOutputDiff()
+    except:
+        print("No characteristic found for the given limits")
+        #If no more upper characteristics can be found, best boomerang differential for the given input has been found
+        parameters["uweight"] = parameters["sweight"]
+        parameters["blockedUpperCharacteristics"].append(upperCharacteristic)
+        parameters["blockedLowerCharacteristics"].clear()
+        return 99
+    upperWeight = parameters["sweight"] #Store optimal weight found for upper trail
+
+    #Keep searching for another optimal lower characteristic, otherwise move on to different upper
+    lowerWeight = parameters["lweight"]
+    start_time = time.time()
+
+    #Calculate weight limit of lower trails
+    if parameters["lweight"] < parameters["wordsize"]/parameters["sboxSize"]:
+        searchLimit = parameters["wordsize"]/parameters["sboxSize"] - parameters["lweight"]
+    else:
+        searchLimit = 1 #+1 means only find optimal lower trails 
+
+    while not search.reachedTimelimit(start_time, parameters["timelimit"]) and \
+        lowerWeight < parameters["lweight"]+searchLimit: 
+
+        #Search Lower Trail
+        lowerCharacteristic = boomerangTrail(cipher, parameters, "lower", beta)
+
+        #Store output difference
+        try:
+            gamma = lowerCharacteristic.getInputDiff()
+            delta = lowerCharacteristic.getOutputDiff()
+        except:
+            print("No characteristic found for the given limits")
+            parameters["blockedUpperCharacteristics"].append(upperCharacteristic)   
+            parameters["blockedLowerCharacteristics"].clear()
+            return 0
+        lowerWeight = parameters["sweight"] #Store optimal weight found for lower trail
+
+        #Block characteristics
+        parameters["blockedLowerCharacteristics"].append(lowerCharacteristic)
+
+        #Check for match
+        switchProb = checkBCT(beta, gamma, parameters, cipher)
+
+        #Successful switch has been found
+        if switchProb != 0:
+            #Fix starting point if it has not been set in boomerang Variables
+            if "X0" not in parameters["boomerangVariables"]:
+                parameters["boomerangVariables"]["X0"] = alpha
+                print("Fixed X0 in boomerang to {}".format(parameters["boomerangVariables"]["X0"]))
+                print("----")
+            #Fix end point if it has not been set in boomerang Variables
+            if "X{}".format(parameters["lowertrail"]) not in parameters["boomerangVariables"]:
+                parameters["boomerangVariables"]["X{}".format(parameters["lowertrail"])] = delta
+                print("Fixed X{} in boomerang to {}".format(parameters["lowertrail"], parameters["boomerangVariables"]["X{}".format(parameters["lowertrail"])]))
+                print("----")
+            #Perform clustering for upper if not done, then cluster lower
+            if diff_upper == 0:
+                diff_upper = boomerangDifferential(cipher, parameters, alpha, beta, upperWeight, "upper")
+            while diff_upper == 0: #TODO: Change later
+                print("Upper clustering failed.")
+                diff_upper = boomerangDifferential(cipher, parameters, alpha, beta, upperWeight, "upper")
+                #continue
+            diff_lower = boomerangDifferential(cipher, parameters, gamma, delta, lowerWeight, "lower")
+            while diff_lower == 0: #TODO: Change later
+                print("Lower clustering failed.")
+                diff_lower = boomerangDifferential(cipher, parameters, gamma, delta, lowerWeight, "lower")
+                #continue
+            boomerangProb += diff_upper*diff_upper*diff_lower*diff_lower*switchProb
+            print("Found boomerang trail: {}, {}, {}".format(math.log(diff_upper, 2), math.log(diff_lower, 2),math.log(switchProb, 2)))
+            print("Boomerang probability: {}".format(math.log(boomerangProb, 2)))
+            print("----")
+        else:
+            print("Switching invalid, search for new boomerang differential")
+            print("----")
+   
+    #After searching for all possible optimal lower trails for the given upper trail, block upper trail
+    print("Completed trail search with boomerang probability of {}".format(math.log(boomerangProb, 2)))
+    #Block upper trail to find another upper trail
+    parameters["blockedUpperCharacteristics"].append(upperCharacteristic)
+    #Clear lower trails because the same lower trails can be matched to a different upper trail
+    parameters["blockedLowerCharacteristics"].clear()
+    parameters["uweight"] = upperWeight
+    return boomerangProb
+
+
 def createBCT(parameters, cipher):
     """
     Create BCT or FBCT - Ensure that these functions are available in cipher model
     """
     s = parameters["sbox"]
     #Create FBCT
-    if parameters["design"] == "gfn" or parameters["design"] == "feistel":
+    if parameters["design"] == "gfn":
         print("Creating FBCT for {}".format(parameters["cipher"]))
         for Di in range(16):
             for Do in range(16):
@@ -402,35 +402,7 @@ def blockInvalidSwitches(beta, parameters, stp_filename):
                             a = "X0[{0}:{1}]".format((parameters["perm"][n]*4)+3, parameters["perm"][n]*4)
                             b = "{}".format(hex(output))
                             blockVariableValue(stp_file, a, b)
-                        #If this is the initial trail, only allow switching probability of 1 (not necessarily the best results)
-                        if ("X{}".format(parameters["lowertrail"]) not in parameters["boomerangVariables"] and parameters["bct"][input][output] != 16): 
-                            a = "X0[{0}:{1}]".format((parameters["perm"][n]*4)+3, parameters["perm"][n]*4)
-                            b = "{}".format(hex(output))
-                            blockVariableValue(stp_file, a, b)
                 n+=2
-        #Lblock
-        if parameters["design"] == "feistel":
-            n = 0
-            nibbles = int(parameters["wordsize"]/parameters["sboxSize"]/2)
-            for x in range(-1, -nibbles, -1):
-                input = int(beta[x],16)
-                #Check if input to BCT is nonzero
-                if input != 0:
-                    #Loop through all BCT outputs for the given input and put into fixedVariables for blocking
-                    for output in range(16):
-                        if parameters["bct"][input][output] == 0:
-                            a = "X0[{0}:{1}]".format((parameters["perm"][n]*4)+3, parameters["perm"][n]*4)
-                            b = "{}".format(hex(output))
-                            blockVariableValue(stp_file, a, b)
-                        #If this is the initial trail, only allow switching probability of 1 (not necessarily the best results)
-                        if ("X{}".format(parameters["lowertrail"]) not in parameters["boomerangVariables"] and parameters["bct"][input][output] != 16): 
-                            a = "X0[{0}:{1}]".format((parameters["perm"][n]*4)+3, parameters["perm"][n]*4)
-                            b = "{}".format(hex(output))
-                            blockVariableValue(stp_file, a, b)
-                n+=1
-
-        #Ensure that beta is not equal to gamma
-        blockVariableValue(stp_file, "X0", beta)
         stpcommands.setupQuery(stp_file)
     return
 
@@ -443,6 +415,7 @@ def checkBCT(beta, gamma, parameters, cipher):
     
     #For GFN like WARP or TWINE (Note: Cannot be used for LBLOCK)
     if parameters["design"] == "gfn":
+        n = 1
         nibbles = int(parameters["wordsize"]/parameters["sboxSize"])
         for x in range(-1, -nibbles, -2):
             input = int(beta[x],16)
@@ -454,21 +427,7 @@ def checkBCT(beta, gamma, parameters, cipher):
                 switchProb = switchProb * parameters["bct"][input][output]/(parameters["sboxSize"]*parameters["sboxSize"])
             else:
                 return 0
-    
-    #For LBlock
-    if parameters["design"] == "feistel":
-        nibbles = int(parameters["wordsize"]/parameters["sboxSize"]/2)
-        for x in range(-1, -nibbles, -1):
-            input = int(beta[x],16)
-            #Calculate position after permutation
-            pos = -parameters["perm"][(-x)-1]-1
-            output = int(gamma[pos],16)
-
-            if parameters["bct"][input][output] != 0:
-                switchProb = switchProb * parameters["bct"][input][output]/(parameters["sboxSize"]*parameters["sboxSize"])
-            else:
-                return 0
-
+            n+=2
     return switchProb
 
 
